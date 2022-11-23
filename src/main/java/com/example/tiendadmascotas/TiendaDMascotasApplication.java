573f56605757928df7cb4e5e483308a7cbeb36f7
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-11-23 15:33:02 -0500

    Initial commit

---
+++ src/main/java/com/example/tiendadmascotas/TiendaDMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendadmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDMascotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaDMascotasApplication.class, args);
+    }
+
+}
