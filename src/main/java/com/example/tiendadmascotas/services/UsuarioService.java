8b2788b1fc9bd6a5741e3590c28e59215885eb90
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-11-23 20:12:48 -0500

    Pequeño avance

---
+++ src/main/java/com/example/tiendadmascotas/services/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.example.tiendadmascotas.services;
+
+import com.example.tiendadmascotas.model.Rol;
+import com.example.tiendadmascotas.model.Usuario;
+
+import java.util.Set;
+
+public interface UsuarioService {
+
+    public Usuario guardarUsuario(Usuario usuario, Rol Roles) throws Exception;
+
+    public Usuario obtenerUsuario(String username);
+
+    public void eliminarUsuario(Long usuarioId);
+}
