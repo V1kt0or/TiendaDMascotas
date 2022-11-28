711a34d7e2964bd18a378ae6dc44e0a18fa75d94
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-11-28 00:06:59 -0500

    Pequeño avance precio

--- src/main/java/com/example/tiendadmascotas/services/PrecioService.java
+++ src/main/java/com/example/tiendadmascotas/services/PrecioService.java
@@ -1,4 +1,13 @@
 package com.example.tiendadmascotas.services;
 
+import com.example.tiendadmascotas.model.Precio;
+
 public interface PrecioService {
+
+    public Precio GuardarPrecio(Precio precio);
+
+    public Precio VerPrecio(Precio precio);
+
+    public Precio EditarPrecio(Precio precio);
+
 }
--- src/main/java/com/example/tiendadmascotas/services/implementation/PrecioServiceImp.java
+++ src/main/java/com/example/tiendadmascotas/services/implementation/PrecioServiceImp.java
@@ -1,4 +1,21 @@
 package com.example.tiendadmascotas.services.implementation;
 
-public class PrecioServiceImp {
+import com.example.tiendadmascotas.model.Precio;
+import com.example.tiendadmascotas.services.PrecioService;
+
+public class PrecioServiceImp implements PrecioService {
+    @Override
+    public Precio GuardarPrecio(Precio precio) {
+        return null;
+    }
+
+    @Override
+    public Precio VerPrecio(Precio precio) {
+        return null;
+    }
+
+    @Override
+    public Precio EditarPrecio(Precio precio) {
+        return null;
+    }
 }
