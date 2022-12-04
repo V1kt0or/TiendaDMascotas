44704026b35bc4c5e42544c1051724f16bea438b
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-12-04 03:15:14 -0500

    Incorporacion imágenes a Productos

--- src/main/java/com/example/tiendadmascotas/controller/ImagenController.java
+++ src/main/java/com/example/tiendadmascotas/controller/ImagenController.java
@@ -3,6 +3,8 @@ package com.example.tiendadmascotas.controller;
 
 import com.example.tiendadmascotas.model.Imagen;
 import com.example.tiendadmascotas.services.ImagenService;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/imagenes")
 @CrossOrigin("*")
@@ -20,21 +24,32 @@ public class ImagenController {
     @Autowired
     private ImagenService imagenService;
 
+
+
     @PostMapping("/")
-    public ResponseEntity<?> guardarArchivo(@RequestParam("file") MultipartFile file) throws Exception {
+    public ResponseEntity<?> guardarArchivo(@RequestParam("file") MultipartFile file, String info) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println(info);
         Imagen imagen = null;
-        imagen  = imagenService.agregarImagen(file);
+        imagen  = imagenService.agregarImagen(file,Long.parseLong(info));
         String URLDescarga = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/descarga/")
                 .path(imagen.getId().toString())
                 .toUriString();
         System.out.println(URLDescarga);
+
         return ResponseEntity.ok(imagen);
     }
 
     @GetMapping("/{imagenId}")
     public Imagen listarImagenPorId(@PathVariable("imagenId") Long imagenId){
         return imagenService.verImagen(imagenId);
+    }
+
+    @GetMapping("/producto/{productoId}")
+    public List<Imagen> listarImagenPorProducto(@PathVariable("productoId") Long productoId){
+        return imagenService.obtenerImagenesDeProducto(productoId);
     }
 
     @GetMapping("/descarga/{imagenId}")
--- src/main/java/com/example/tiendadmascotas/services/implementation/ImagenServiceImp.java
+++ src/main/java/com/example/tiendadmascotas/services/implementation/ImagenServiceImp.java
@@ -1,12 +1,15 @@
 package com.example.tiendadmascotas.services.implementation;
 
 import com.example.tiendadmascotas.model.Imagen;
+import com.example.tiendadmascotas.model.Producto;
 import com.example.tiendadmascotas.repository.ImagenRepository;
 import com.example.tiendadmascotas.services.ImagenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 public class ImagenServiceImp implements ImagenService {
@@ -15,14 +18,16 @@ public class ImagenServiceImp implements ImagenService {
     private ImagenRepository imagenRepository;
 
     @Override
-    public Imagen agregarImagen(MultipartFile file) throws Exception {
+    public Imagen agregarImagen(MultipartFile file, Long idProducto) throws Exception {
         String Nombre = StringUtils.cleanPath(file.getOriginalFilename());
         try {
             if (Nombre.contains("..")){
                 throw new Exception("Nombre de imagen no válido" + Nombre);
             }
-
+            Producto producto= new Producto();
+            producto.setId(idProducto);
             Imagen imagen = imagenRepository.save(Imagen.builder()
+                            .producto(producto)
                     .nombreImagen(Nombre)
                     .tipoImagen(file.getContentType())
                     .dataImagen(file.getBytes()).build());
@@ -45,6 +50,13 @@ public class ImagenServiceImp implements ImagenService {
                         () -> new Exception("Imagen no encontrado :" + imagenId)
                 );
 
+    }
+
+    @Override
+    public List<Imagen> obtenerImagenesDeProducto(Long productoId) {
+        Producto  producto =new Producto();
+        producto.setId(productoId);
+        return imagenRepository.findByProducto(producto);
     }
 
     @Override
